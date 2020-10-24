adb14f1ac64fd9dcf3a629926cd87585fbceafab
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2020-10-24 18:31:43 +0700

    Create GetdataHardware.vue
    get api form axios Async&Await

---
+++ GetdataHardware.vue
@@ -0,0 +1,31 @@
+ created() {
+    this.getTemp();
+    this.setIntervalId = setInterval(() => {
+      this.getTemp();
+    }, 3000);
+  },
+  beforeDestroy() {
+    clearInterval(this.setIntervalId);
+  },
+  
+
+  methods: {
+    
+    async getTemp() {
+      // this.loading = true;
+      try {
+        const dht = await axios.get("http://192.168.137.23/getscale");
+        this.temp = dht.data.temp;
+        this.hum = dht.data.hum;
+        
+
+        this.loading = false;
+        console.log("temp:");
+        console.log(this.temp);
+        console.log("Hum");
+        console.log(this.hum);
+      } catch (err) {
+        console.log(err); // handle errors here...
+      }
+    },
+ 
