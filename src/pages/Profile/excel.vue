7c1e40f4ddf991ca5ebb96a383b79720894e6205
Pupan Phonkaew <43066870+Pupan-cpe@users.noreply.github.com>
2020-10-27 11:49:58 +0700

    update v1.05

---
+++ src/pages/Profile/excel.vue
@@ -0,0 +1,32 @@
+<template>
+  <div style="text-align:center;" >
+    <h1>Export JSON to Excel</h1>
+    <v-btn @click="onExport" color="success" >Export</v-btn> <!-- เพิ่มปุ่ม Export -->
+  </div>
+</template>
+
+<script>
+import XLSX from 'xlsx' // import xlsx
+export default {
+  name: 'app',
+  data(){
+    return {
+      // ข้อมูล JSON ทีต้องการ Export
+      json : [
+        { name: 'pupan', id: '21' },
+        { name: 'Jonh', id: '25' },
+        { name: 'James', id: '17' },
+      ]
+    }
+  },
+  methods: {
+    // เมื่อกดปุ่มจะทำการสร้างไฟล์ xcel ด้วย xlsx
+    onExport() {
+      const dataWS = XLSX.utils.json_to_sheet(this.json)
+      const wb = XLSX.utils.book_new()
+      XLSX.utils.book_append_sheet(wb, dataWS)
+      XLSX.writeFile(wb,'export.xlsx')
+    },
+  }
+}
+</script>
